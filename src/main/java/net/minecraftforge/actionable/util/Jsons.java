b0dea855c1ad6f5036b8d862e1ba88b643901464
Matyrobbrt <matyrobbrt@gmail.com>
2023-02-14 21:56:58 +0200

    Add license headers

--- src/main/java/net/minecraftforge/actionable/util/Jsons.java
+++ src/main/java/net/minecraftforge/actionable/util/Jsons.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.minecraftforge.actionable.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
